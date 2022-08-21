ea7d335a7b38322c27e6a527b046c9ad6a9dec92
Ivan Churakov <ivanchurakof@gmail.com>
2022-08-21 10:38:58 +0300

    [+] Timeline

---
+++ src/components/Main/Timeline.tsx
@@ -0,0 +1,13 @@
+// @flow
+import * as React from 'react';
+
+type Props = {
+
+};
+export const Timeline = (props: Props) => {
+    return (
+        <div className=''>
+
+        </div>
+    );
+};
