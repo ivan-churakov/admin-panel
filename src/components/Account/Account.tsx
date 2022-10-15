317698079f5b92a642864253065d1ef7f66c60b7
Ivan Churakov <43320869+ivan-churakov@users.noreply.github.com>
2022-10-15 13:09:17 +0300

    [~] Delete Props

--- src/components/Account/Account.tsx
+++ src/components/Account/Account.tsx
@@ -2,10 +2,7 @@
 import * as React from 'react';
 import {VscAccount} from 'react-icons/vsc';
 
-type Props = {
-
-};
-export const Account = (props: Props) => {
+export const Account = () => {
     return (
         <div className="flex gap-3 items-center border-b-2 border-l-2 border-gray-600/50 p-5 h-24">
             <VscAccount className="w-8 h-8" />
